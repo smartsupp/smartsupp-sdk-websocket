102268fc13a502e8b61f3e4c74004d3998337eb8
Dusan Kmet <morlokdk@gmail.com>
2021-03-08 17:08:39 +0100

    use pages provider

--- scripts/docs-deploy.sh
+++ scripts/docs-deploy.sh
@@ -7,8 +7,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-ssh-keyscan github.com > ~/.ssh/known_hosts
-ssh-add ~/.ssh/id_rsa
 git push -f git@github.com:smartsupp/smartsupp-sdk-websocket.git master:gh-pages
 
 cd -
