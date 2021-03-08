5b91faa6f3977126c3ff0677b8d00c2a3e6fd123
Dusan Kmet <morlokdk@gmail.com>
2021-03-08 16:57:48 +0100

    travis deploy

--- scripts/docs-deploy.sh
+++ scripts/docs-deploy.sh
@@ -7,6 +7,8 @@ git init
 git add -A
 git commit -m 'deploy'
 
+ssh-keyscan github.com > ~/.ssh/known_hosts
+ssh-add ~/.ssh/id_rsa
 git push -f git@github.com:smartsupp/smartsupp-sdk-websocket.git master:gh-pages
 
 cd -
